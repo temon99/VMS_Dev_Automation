0b989365be9f4f14c44483195eaa45be2c8aa7ff
shikder-Klikit <ehatesham.islam@vivasoftltd.com>
2025-03-21 15:31:53 +0600

    setup 2

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,6 +1,6 @@
 import './VMS/VMS Login'
-import 'VMS/User Create'
-import 'cypress-xpath';
+import './VMS/User Create'
+import 'xpath_cypress';
 import 'cypress-file-upload';
 import '@faker-js/faker'
 import 'cypress-wait-until';
