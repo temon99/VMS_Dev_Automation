1fe0dcd16589969b2742ffc9c94946ad7df6611b
shikder-Klikit <ehatesham.islam@vivasoftltd.com>
2025-03-21 15:25:55 +0600

    setup 1

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,3 +1,7 @@
-import ('./VMS/VMS Login')
-import('./VMS/User Create')
+import './VMS/VMS Login'
+import 'VMS/User Create'
 import 'cypress-xpath';
+import 'cypress-file-upload';
+import '@faker-js/faker'
+import 'cypress-wait-until';
+import "allure-cypress";
--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -1,47 +1,6 @@
-// ***********************************************************
-// This example support/e2e.js is processed and
-// loaded automatically before your test files.
-//
-// This is a great place to put global configuration and
-// behavior that modifies Cypress.
-//
-// You can change the location of this file or turn off
-// automatically serving support files with the
-// 'supportFile' configuration option.
-//
-// You can read more here:
-// https://on.cypress.io/configuration
-// ***********************************************************
-
-// Import commands.js using ES2015 syntax:
 import './commands'
 
-// Alternatively you can use CommonJS syntax:
-// require('./commands')
-import './VMS/VMS Login'
-// ***********************************************************
-// This example support/e2e.js is processed and
-// loaded automatically before your test files.
-//
-// This is a great place to put global configuration and
-// behavior that modifies Cypress.
-//
-// You can change the location of this file or turn off
-// automatically serving support files with the
-// 'supportFile' configuration option.
-//
-// You can read more here:
-// https://on.cypress.io/configuration
-// ***********************************************************
-
-// Import commands.js using ES2015 syntax:
-import './commands'
-
-// Alternatively you can use CommonJS syntax:
-// require('./commands')
 Cypress.on('uncaught:exception', (err, runnable) => {
-    // returning false here prevents Cypress from
-    // failing the test
     return false
 })
 const origLog1 = Cypress.log
@@ -54,4 +13,3 @@ Cypress.log = function (opts, ...other) {
     }
     return origLog1(opts, ...other)
 }
-import './commands'
