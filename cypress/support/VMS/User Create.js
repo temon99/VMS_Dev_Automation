f4076fa65c6d7acc36dc4c3094eeda0eb7df7122
Tanzim Emon <tanzim.emon@vivasoftltd.com>
2025-04-07 15:00:18 +0600

    User Delete

--- cypress/support/VMS/User Create.js
+++ cypress/support/VMS/User Create.js
@@ -13,10 +13,10 @@ const phoneNumber = faker.phone.number({ style: 'national' })
 const currentAddress = faker.location.streetAddress({ useFullAddress: true })
 Cypress.Commands.add('User_Create',()=>{
     cy.contains('Users').click()
-    cy.contains('Add User').click()              //New User Create
-    cy.contains('First Name'). type(firstname)   // User First name
-    cy.contains('Last Name'). type(lastName)      // User Last name
-    //cy.xpath('//div[@class=\'flex items-center justify-center w-full h-full\']').selectFile('cypress/fixtures/VMS/Images/David_Backham.jpg');
+    cy.contains('Add User').click()                                                                       //New User Create
+    cy.contains('First Name'). type(firstname)                                                           // User First name
+    cy.contains('Last Name'). type(lastName)                                                             // User Last name
+    //cy.xpath('//div[@class=\'flex items-center justify-center w-full h-full\']').selectFile(cypress/fixtures/VMS/Images/David_Backham.jpg);
     //cy.wait(8000)
     cy.contains ('Company Name'). type(companyName)                                                      // User Company Name
     cy.contains('Select department').click()                                                             // User Department Select
@@ -28,7 +28,11 @@ Cypress.Commands.add('User_Create',()=>{
     cy.contains('Current Address') .type(currentAddress)                                                  // Current Address
     cy.contains ('Select assigned vehicle').click()                                                       // Vehicle Name
     cy.xpath("//div[@role='presentation']").contains('Car One').click()
-    cy.contains('Select assigned driver').click()                                                           // Driver Name
+    cy.contains('Select assigned driver').click()                                                         // Driver Name
     cy.xpath("//div[@role='presentation']").contains('Abdul Malek').click()
-    cy.contains ('Save').click()
+    cy.contains ('Save').click(). wait (2000)
+    cy.get('.gap-3 > .items-center > .relative > .flex'). type(firstname ).wait (2000)
+    //cy.xpath("//tbody/tr[1]").click()
+    cy.get('.ml-0 > .inline-flex').click()
+    cy.xpath("//button[normalize-space()='Delete']") .wait(2000).click().wait(2000)
 })
